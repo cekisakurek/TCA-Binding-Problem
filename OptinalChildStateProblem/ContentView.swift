bb96c7f262d45ea7306600f1b455eb6249ca2381
Cihan Kisakurek <c.kisakuerek@appdev.de>
2022-11-21 17:41:27 +0100

    Initial Commit

---
+++ OptinalChildStateProblem/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OptinalChildStateProblem
+//
+//  Created by Cihan Kisakurek on 21.11.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OptinalChildStateProblem/OptinalChildStateProblemApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptinalChildStateProblemApp.swift
+//  OptinalChildStateProblem
+//
+//  Created by Cihan Kisakurek on 21.11.22.
+//
+
+import SwiftUI
+
+@main
+struct OptinalChildStateProblemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
